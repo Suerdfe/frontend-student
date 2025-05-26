1db4be7a4b0e6472300724500ad353fdfe7e3a9c
Osipov_Dmitriy <o.dima01@bk.ru>
2025-05-26 19:46:52 +0300

    task 09/01Ñ-02

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,18 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const resultArray = [];
+    for (i = 0; i < array.length; i++) {
+        resultArray.push(callback(array[i], i));
+    }
+    return resultArray
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+const doubledNumbers = map(numbers, (element, index) => {
+    return element * 2
+})
+
+console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
