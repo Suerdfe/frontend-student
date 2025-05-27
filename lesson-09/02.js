b81e3466c0cdf68192fe6597be64ee69fb151dfd
Osipov_Dmitriy <o.dima01@bk.ru>
2025-05-27 21:19:49 +0300

    task 09/02

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -38,4 +38,4 @@ const doubledNumbers = map(numbers, (element, index) => {
     return element * 2
 })
 
-console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
+console.log(doubledNumbers); // Должен вывести: [2, 4, 6, 8, 10]
